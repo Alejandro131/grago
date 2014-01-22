e26a58395decce4a47dfb3b5b65cfb97febb0886
Alejandro131 <alex131_d@yahoo.com>
2014-01-22 16:19:12 +0200

    added max flow

--- max_flow.go
+++ max_flow.go
@@ -1,7 +1,100 @@
 package grago
 
+// Helper function for the max flow algorithm
+func flowDFS(startNode string, flowPath *[]string, newFlow *bool, marked *map[string]bool, sink string, adjacency *map[string]map[string]int, flow *map[string]map[string]int) {
+	if *newFlow {
+		return
+	}
+	
+	if startNode == sink {
+		*newFlow = true
+		updateFlow(flowPath, adjacency, flow)
+	} else {
+		for node := range *adjacency {
+			if !(*marked)[node] && (*adjacency)[startNode][node] > 0 {
+				(*marked)[node] = true
+				
+				*flowPath = append(*flowPath, node)
+				flowDFS(node, flowPath, newFlow, marked, sink, adjacency, flow)
+				*flowPath = (*flowPath)[:len(*flowPath) - 1]
+				
+				if *newFlow {
+					return
+				}
+			}
+		}
+		
+	}
+}
+
+// Helper function for the max flow algorithm
+func updateFlow(flowPath *[]string, adjacency *map[string]map[string]int, flow *map[string]map[string]int) {
+	increaseFlow := 0
+	
+	if len(*flowPath) >= 2 {
+		increaseFlow = (*adjacency)[(*flowPath)[0]][(*flowPath)[1]]
+	}
+
+	for index := 1; index < len(*flowPath) - 1; index++ {
+		if increaseFlow > (*adjacency)[(*flowPath)[index]][(*flowPath)[index + 1]] {
+			increaseFlow = (*adjacency)[(*flowPath)[index]][(*flowPath)[index + 1]]
+		}
+	}
+	
+	for index := 0; index < len(*flowPath) - 1; index++ {
+		(*flow)[(*flowPath)[index]][(*flowPath)[index + 1]] += increaseFlow
+		(*flow)[(*flowPath)[index + 1]][(*flowPath)[index]] -= increaseFlow
+		(*adjacency)[(*flowPath)[index]][(*flowPath)[index + 1]] -= increaseFlow
+		(*adjacency)[(*flowPath)[index + 1]][(*flowPath)[index]] += increaseFlow
+	}
+}
+
 // Returns the maximum ammount that can flow from
 // the source to the sink for 1 period of time
 // according to the min-cut max-flow algorithm.
 func (g *Graph) MaxFlow(source string, sink string) int {
+	flow := make(map[string]map[string]int)
+	adjacency := make(map[string]map[string]int)
+	
+	for _, startNode := range g.Nodes() {
+		adjacency[startNode] = make(map[string]int)
+		flow[startNode] = make(map[string]int)
+		
+		for _, endNode := range g.Nodes() {
+			flow[startNode][endNode] = 0
+			adjacency[startNode][endNode] = 0
+		}
+	}
+	
+	for _, startNode := range g.Nodes() {
+		for endNode, weight := range g.nodes[startNode].Adjacent {
+			adjacency[startNode][endNode] = weight
+		}
+	}
+	
+	marked := make(map[string]bool)
+	for _, node := range g.Nodes() {
+		marked[node] = false
+	}
+	newFlow := true
+	
+	
+	for newFlow {
+		for _, node := range g.Nodes() {
+			marked[node] = false
+		}
+		newFlow = false
+		marked[source] = true
+		flowPath := []string{source}
+		
+		flowDFS(source, &flowPath, &newFlow, &marked, sink, &adjacency, &flow)
+	}
+	
+	result := 0
+	
+	for _, node := range g.Nodes() {
+		result += flow[node][sink]
+	}
+	
+	return result
 }
--- max_flow_test.go
+++ max_flow_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func createGraph() *Graph {
+func createGraphmf() *Graph {
 	graph := NewGraph(false, true, false)
 	graph.AddNode("alpha")
 	graph.AddLink("2", "3", 2)
@@ -15,8 +15,31 @@ func createGraph() *Graph {
 	return graph
 }
 
+func createGraphmf2() *Graph {
+	graph := NewGraph(true, true, false)
+	graph.AddLink("s", "o", 3)
+	graph.AddLink("s", "p", 3)
+	graph.AddLink("o", "p", 2)
+	graph.AddLink("p", "r", 2)
+	graph.AddLink("o", "q", 3)
+	graph.AddLink("q", "r", 4)
+	graph.AddLink("q", "t", 2)
+	graph.AddLink("r", "t", 3)
+	return graph
+}
+
+func createGraphmf3() *Graph {
+	graph := NewGraph(true, true, false)
+	graph.AddLink("s", "u", 10)
+	graph.AddLink("s", "v", 5)
+	graph.AddLink("u", "v", 15)
+	graph.AddLink("v", "t", 10)
+	graph.AddLink("u", "t", 5)
+	return graph
+}
+
 func ExampleMaxFlow_Graph() {
-	graph := createGraph()
+	graph := createGraphmf()
 
 	fmt.Println(graph.MaxFlow("2", "5"))
 	fmt.Println(graph.MaxFlow("3", "4"))
@@ -29,19 +52,31 @@ func ExampleMaxFlow_Graph() {
 }
 
 func TestMaxFlow(t *testing.T) {
-	if createGraph().MaxFlow("2", "5") != 7 {
+	if createGraphmf().MaxFlow("2", "5") != 7 {
 		t.Fail()
 	}
 }
 
 func TestMaxFlow2(t *testing.T) {
-	if createGraph().MaxFlow("3", "4") != 10 {
+	if createGraphmf().MaxFlow("3", "4") != 10 {
 		t.Fail()
 	}
 }
 
 func TestMaxFlow3(t *testing.T) {
-	if createGraph().MaxFlow("alpha", "4") != 0 {
+	if createGraphmf().MaxFlow("alpha", "4") != 0 {
+		t.Fail()
+	}
+}
+
+func TestMaxFlow4(t *testing.T) {
+	if createGraphmf2().MaxFlow("s", "t") != 5 {
+		t.Fail()
+	}
+}
+
+func TestMaxFlow5(t *testing.T) {
+	if createGraphmf3().MaxFlow("s", "t") != 15 {
 		t.Fail()
 	}
 }
